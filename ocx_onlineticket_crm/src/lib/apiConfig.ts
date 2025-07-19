c186498e2be040a1199b13b3a6096e81478c74af
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2025-07-20 00:51:24 +0700

    Update apiConfig.ts

--- ocx_onlineticket_crm/src/lib/apiConfig.ts
+++ ocx_onlineticket_crm/src/lib/apiConfig.ts
@@ -1,2 +1,4 @@
+// // API base URL config
+// export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'; 
 // API base URL config
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'; 
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://32a8d2edb3b3.ngrok-free.app'; 
