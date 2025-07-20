586e7700db06b1f55fb9e0f57e5d38f96f960815
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2025-07-20 13:09:27 +0700

    Update apiConfig.ts

--- ocx_onlineticket_crm/src/lib/apiConfig.ts
+++ ocx_onlineticket_crm/src/lib/apiConfig.ts
@@ -1,4 +1,4 @@
 // // API base URL config
 // export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:3000'; 
 // API base URL config
-export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://32a8d2edb3b3.ngrok-free.app'; 
+export const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://prime-fox-lively.ngrok-free.app'; 
