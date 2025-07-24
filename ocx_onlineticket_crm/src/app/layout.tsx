cfb049db9b9afb4990e29fcea5629c00e14900bf
Triết Nguyễn Phạm <triet.pnc@gmail.com>
2025-07-24 17:12:24 +0700

    Update layout.tsx

--- ocx_onlineticket_crm/src/app/layout.tsx
+++ ocx_onlineticket_crm/src/app/layout.tsx
@@ -28,14 +28,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <meta name="application-name" content="OCX Admin" />
         <meta name="description" content="Nền tảng quản trị bán vé đa tổ chức, bảo mật, realtime, do Howls Studio phát triển." />
         <link rel="icon" href="/client_logo_ss3.jpg" />
-        <meta property="og:image" content="/client_logo_ss3.jpg" />
+        <meta property="og:image" content="https://admin.otcayxe.com/client_logo_ss3.jpg" />
         <meta name="author" content="Howls Studio" />
         <meta name="provider" content="Howls Studio" />
         <meta property="og:site_name" content="Howls Studio" />
         <meta property="og:brand" content="Howls Studio" />
-        <meta property="og:logo" content="/sponsor_logo_1.png" />
+        <meta property="og:logo" content="https://admin.otcayxe.com/sponsor_logo_1.png" />
         <meta property="og:title" content="OCX Admin - Howls Studio" />
         <meta property="og:description" content="Nền tảng quản trị bán vé đa tổ chức, bảo mật, realtime, do Howls Studio phát triển." />
+        <meta property="og:url" content="https://admin.otcayxe.com/" />
+        <meta property="og:type" content="website" />
       </Head>
       <html lang="en" className="dark bg-gray-900">
         <body
